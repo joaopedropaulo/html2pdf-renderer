0221a5472233dd78bb37a6dc6f7160548bfb5fbb
João Pedro Paulo <joaopospaulo@gmail.com>
2020-09-12 15:11:46 +0100

    Adds possibility to provide an HTML template, a JSON input and render a PDF. Adds endpoint to rendered an HTMl page based on the provided template and put JSON.

--- src/routes/api/generate.js
+++ src/routes/api/generate.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const isEmptyObject = require("../../utils/isEmptyObject");
 const Mustache = require("mustache");
-const pdf = require("html-pdf");
 const multer = require("multer");
+const pdf = require("html-pdf");
+var wkhtmltopdf = require("wkhtmltopdf");
+const puppeteer = require("puppeteer");
 var upload = multer();
 
-// @route POST api/generate
+// @route POST api/generate/html
 // @desc Generate resume
 // @access Public
-router.post("/", upload.none(), async (req, res, next) => {
+router.post("/html", upload.none(), async (req, res, next) => {
   if (
     isEmptyObject(req.body) ||
     req.body.template === null ||
@@ -28,16 +30,53 @@ router.post("/", upload.none(), async (req, res, next) => {
 
   try {
     var renderedHtml = Mustache.render(template, input);
-    pdf.create(renderedHtml).toStream((err, stream) => {
-      if (err) return res.end(err.stack);
-      res.setHeader("Content-type", "application/pdf");
-      stream.pipe(res);
-    });
+    res.setHeader("Content-type", "text/html");
+    return res.send(renderedHtml);
   } catch (err) {
     console.error(err.message);
     return res
       .status(500)
       .json({ errors: [{ msg: "Internal Server Error." }] });
+  }
+});
+
+// @route POST api/generate/pdf
+// @desc Generate resume
+// @access Public
+router.post("/pdf", upload.none(), async (req, res, next) => {
+  if (
+    isEmptyObject(req.body) ||
+    req.body.template === null ||
+    req.body.input === null ||
+    isEmptyObject(req.body.template) ||
+    isEmptyObject(req.body.input)
+  ) {
+    return res.status(400).json({
+      error:
+        "The HTML template and corresponding inputs (JSON) need to be provided.",
+    });
+  }
+
+  var template = req.body.template;
+  var input = JSON.parse(req.body.input);
+
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  try {
+    var renderedHtml = Mustache.render(template, input);
+
+    const page = await browser.newPage();
+    await page.setContent(renderedHtml);
+    var pdf = await page.pdf({ format: "A4" });
+
+    res.setHeader("Content-type", "application/pdf");
+    return res.status(200).send(pdf);
+  } catch (err) {
+    console.error(err.message);
+    return res
+      .status(500)
+      .json({ errors: [{ msg: "Internal Server Error." }] });
+  } finally {
+    await browser.close();
   }
 });
 
